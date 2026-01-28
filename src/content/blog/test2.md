4728cbb7567c5d0455c3cddc8cc58584edb0ddbc
lukebrown14-code <lukebrown14@proton.me>
2026-01-28 11:19:23 +0000

    Add Simple Astro.js Blog

---
+++ src/content/blog/test2.md
@@ -0,0 +1,9 @@
+---
+title: "The greatest post of all time"
+description: "Ben"
+---
+
+Here is my _great_ post!
+
+
+Your welocme it works
---
+++ src/content/config.ts
@@ -0,0 +1,11 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    category: z.string().optional(),
+  }),
+});
+
+export const collections = { blog };
